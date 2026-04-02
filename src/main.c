ca03e2e1b7eec946b012d2fa42e18b71e97113ba
lee <seoyedogu@naver.com>
2026-04-02 22:13:09 +0900

    feat: week1 - basic ptrace example

--- src/main.c
+++ src/main.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
-#include "debugger.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
-int main(void) {
-    debugger_run();
-    return 0;
+int main(int argc, char *argv[])
+{
+    if (argc <2) {
+        printf("사용법 : %s <프로그램>\n", argv[0]);
+        return 1;
+    }
+    pid_t pid = fork();
+    
+    if (pid == 0) {
+        ptrace(PTRACE_TRACEME, 0, 0, 0);
+        raise(SIGSTOP);
+        execv(argv[1], &argv[1]);
+        perror("execv 실패");
+        exit(1);
+    } else {
+        int status;
+        while(1) {
+            waitpid(pid, &status, 0);   
+
+            if (WIFSTOPPED(status)) {
+                int sig = WSTOPSIG(status);
+                printf("[+] chil sstopped at signal %d\n", sig);
+                ptrace(PTRACE_CONT, pid, 0, 0);
+            } else if (WIFEXITED(status)) {
+                printf("[+] child exited (code: %d)\n", WEXITSTATUS(status));
+                break;
+            } else if (WIFSIGNALED(status)) {
+                printf("[!] child killed by signal %d\n", WTERMSIG(status));
+                break;
+            }
+        }
+    }
 }
--- tests/hello.c
+++ tests/hello.c
@@ -1,13 +1,6 @@
-/* 디버거 테스트용 바이너리 */
 #include <stdio.h>
 
-void greet(void) {
-    printf("Hello from greet()\n");
-}
-
 int main(void) {
-    printf("Start\n");
-    greet();
-    printf("End\n");
+    printf("Hello from target!\n");
     return 0;
 }
