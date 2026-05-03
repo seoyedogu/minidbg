4142447765d9776b21bc7e078411f387e125982f
lee <seoyedogu@naver.com>
2026-05-03 20:44:24 +0900

    feat: week2 - ptrace-basic

--- tests/hello.c
+++ tests/hello.c
@@ -1,6 +1,10 @@
+// target.c
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello from target!\n");
+int main() {
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+    printf("result: %d\n", c);
     return 0;
 }
